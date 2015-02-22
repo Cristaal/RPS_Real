0e3e5b1483154eef369dc0cbd38a7e54a76d6fcf
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-21 22:51:16 -0800

    refactor code

--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -3,46 +3,45 @@ require('rps')
 
 describe('String#beats?') do
   it("returns true if rock is the object and scissors is the argument") do
-    expect("rock".beats?("scissors")).to(eq(true))
+    expect("rock".beats?("scissors")).to(eq("Player 1 wins"))
   end
 
   describe('String#beats?') do
     it("returns false if rock is the object and paper is the argument") do
-      expect("rock".beats?("paper")).to(eq(false))
+      expect("rock".beats?("paper")).to(eq("Player 2 wins"))
     end
 
     describe('String#beats?') do
       it("returns false if rock is the object and rock is the argument") do
-        expect("rock".beats?("rock")).to(eq(false))
+        expect("rock".beats?("rock")).to(eq("draw"))
       end
 
       describe('String#beats?') do
         it("returns false if paper is the object and scissors is the argument") do
-          expect("paper".beats?("scissors")).to(eq(false))
+          expect("paper".beats?("scissors")).to(eq("Player 2 wins"))
         end
 
         describe('String#beats?') do
           it("returns true if paper is the object and rock is the argument") do
-            expect("paper".beats?("rock")).to(eq(true))
+            expect("paper".beats?("rock")).to(eq("Player 1 wins"))
           end
           describe('String#beats?') do
             it("returns false if paper is the object and paper is the argument") do
-              expect("paper".beats?("paper")).to(eq(false))
+              expect("paper".beats?("paper")).to(eq("draw"))
             end
             describe('String#beats?') do
               it("returns false if scissors is the object and rock is the argument") do
-                expect("scissors".beats?("rock")).to(eq(false))
+                expect("scissors".beats?("rock")).to(eq("Player 2 wins"))
               end
               describe('String#beats?') do
                 it("returns true if scissors is the object and paper is the argument") do
-                  expect("scissors".beats?("paper")).to(eq(true))
+                  expect("scissors".beats?("paper")).to(eq("Player 1 wins"))
                 end
                 describe('String#beats?') do
                   it("returns false if scissors is the object and scissors is the argument") do
-                    expect("scissors".beats?("scissors")).to(eq(false))
+                    expect("scissors".beats?("scissors")).to(eq("draw"))
                   end
                 end
-
               end
             end
           end
