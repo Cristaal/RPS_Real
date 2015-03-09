b55c0fc0fae13154f6e3adfc9b70e7bc5f5abd7c
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-09 16:06:31 -0700

    refactor code

--- app.rb
+++ app.rb
@@ -7,11 +7,11 @@ get '/' do
   erb(:form)
 end
 
-get ('/answer') do
+get '/answer' do
   @player1 = params.fetch('player1')
   @player2 = params.fetch('player2')
   results = params.fetch('player1').beats?(params.fetch('player2'))
-    if @player1.empty? || @player2.empty
+    if @player1.empty? || @player2.empty?
       @results = "Please enter rock, paper, or scissors"
     elsif results == true
       @results = "Player 1 wins"
